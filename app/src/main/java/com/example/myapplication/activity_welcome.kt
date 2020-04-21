bfd15d931a0ef4af0dacc29b3e0955bf8d4f6c89
behdax <behdad.pakzad@yahoo.com>
2020-04-22 04:17:48 +0430

    welcome page

--- app/src/main/java/com/example/myapplication/activity_welcome.kt
+++ app/src/main/java/com/example/myapplication/activity_welcome.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+    getstartedbutton.setOnClickListener {
+        val leagueintent=Intent(this,Leagueactivity::class.java)
+        startActivity(leagueintent)
+    }
     }
 }

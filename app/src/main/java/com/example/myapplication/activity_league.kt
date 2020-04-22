449886ff8c5876f3295219a37faddad236618390
behdax <behdad.pakzad@yahoo.com>
2020-04-23 01:40:24 +0430

    welcome page

--- app/src/main/java/com/example/myapplication/activity_league.kt
+++ app/src/main/java/com/example/myapplication/activity_league.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class Leagueactivity : AppCompatActivity() {
+class Leagueactivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+    fun leagueNextClicked(view: View){
+        val intentnextleague=Intent(this,SkillActivity::class.java)
+        startActivity(intentnextleague)
+
     }
 }
